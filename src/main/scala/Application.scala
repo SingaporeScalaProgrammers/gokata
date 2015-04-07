98994783b26a9429019695e07bbae1e0d2acc810
Damien Neveu <damien@redmart.com>
2015-04-07 09:08:11 +0800

    First draft

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,3 +1,4 @@
+import sun.jvm.hotspot.debugger.cdbg.PointerType
 
 object Application {
 
@@ -6,3 +7,53 @@ object Application {
   }
 
 }
+
+sealed trait Point
+sealed trait Color extends Point
+
+case object Empty extends Point
+
+case object White extends Color
+case object Black extends Color
+
+case class Game(val board: Array[Array[Point]], nextColor: Color) {
+  def isEmpty: Boolean = board.forall(isEmptyRow)
+
+  private def isEmptyRow(row: Array[Point]) = row.forall(_ == Empty)
+
+  def afterMove(nextColor: Color, row: Int, col: Int): Option[Game] = nextColor match {
+    case White if isEmpty => None
+    case _ => {
+      if (isInBounds(row, col) &&
+        isBlankPoint(row, col) &&
+        isCorrectMove(nextColor)) {
+
+        val updatedBoard = board.updated(row, board(row).updated(col, nextColor))
+        Some(Game(updatedBoard, otherColor(nextColor)))
+      }
+      else None
+    }
+  }
+
+  private def otherColor(color: Color): Color = color match {
+    case Black => White
+    case White => Black
+  }
+
+  private def isCorrectMove(proposedColor: Point) =
+    proposedColor == nextColor
+
+  private def isBlankPoint(row: Int, col: Int): Boolean =
+    board(row)(col) == Empty
+
+  private def isInBounds(row: Int, col: Int): Boolean =
+    row >= 0 && row < size && col >= 0 && col < size
+
+  private def size =
+    board.length
+}
+
+object Game {
+  def apply(size: Int): Game = Game(Array.fill[Point](size,size) { Empty }, Black)
+}
+
--- src/test/scala/GokataSpec.scala
+++ src/test/scala/GokataSpec.scala
@@ -2,9 +2,46 @@ import org.scalatest._
 
 
 
-class GokataSpec extends FlatSpec with Matchers{
+class GokataSpec extends FlatSpec with ShouldMatchers with GivenWhenThen with BeforeAndAfter {
 
-  "1. The board" should "be empty at the start of the game" in {
-    pending
+  var game: Game = _
+
+  before {
+    game = Game(5)
   }
+
+  "The board" should "be empty at the start of the game" in {
+    game.isEmpty shouldBe true
+  }
+
+  "A move" should "be inside the board" in {
+    game.afterMove(Black, -1, -1) shouldBe None
+    game.afterMove(Black, 6, 5) shouldBe None
+  }
+
+  "A black move inside the board on a new game" should "give a new game state" in {
+    game.afterMove(Black, 1, 1) should not be None
+    game.afterMove(White, 1, 1) shouldBe None
+  }
+
+  "After at least a move, isEmpty" should " be false" in {
+    gameAfterOneMove.isEmpty shouldBe false
+  }
+
+  "A move" should "be played on an empty point" in {
+    gameAfterOneMove.afterMove(White, 1, 1) shouldBe None
+    gameAfterOneMove.afterMove(White, 1, 2) should not be None
+  }
+
+  "Moves" should "alternate from black to white, etc.." in {
+    gameAfterOneMove.afterMove(Black, 1, 2) shouldBe None
+  }
+
+  def gameAfterOneMove: Game = {
+    val gameAfterOneMove = game
+      .afterMove(Black, 1, 1)
+      .get
+    gameAfterOneMove
+  }
+
 }
